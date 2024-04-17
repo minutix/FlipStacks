e2f49c740298c3d564f592403800d0c707291a08
libewa <libewa.git@icloud.com>
2024-04-17 18:58:26 +0200

    add license notices

--- FlipStacks/ArrayHasAnyMoreValidCards.swift
+++ FlipStacks/ArrayHasAnyMoreValidCards.swift
@@ -4,6 +4,10 @@
 //
 //  Created by Linus Warnatz on 16.04.24.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//    License, v. 2.0. If a copy of the MPL was not distributed with this
+//    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 
 import Foundation
 
